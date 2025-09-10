85d14394bf7eaae135284996d813ad7d9610c701
Jagjeevan Nahal <nahal.jagjeevan@gmail.com>
2025-09-10 16:36:18 -0700

    changed api_key importing logic to be updated easier

--- riot_api_client.py
+++ riot_api_client.py
@@ -1,10 +1,13 @@
 # src/riot_api_client.py
 import os
 import requests
+from dotenv import load_dotenv
+
+# Load variables from .env and override anything set globally
+load_dotenv(override=True)
 
 API_KEY = os.environ["RIOT_API_KEY"]
 BASE_URL = "https://americas.api.riotgames.com"  # or the appropriate region
-
 
 def get_puuid(game_name: str, tag_line: str) -> str:
     url = f"{BASE_URL}/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}" # Game name refers to the summoner name
